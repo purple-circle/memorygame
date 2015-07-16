3f075f9612f9460f5e33d01ab1cb614545a29212
Juha Tauriainen <juha@bin.fi>
2015-07-16 22:44:37 +0300

    Matching

--- game.js
+++ game.js
@@ -12,24 +12,52 @@
     var gameBoard = board.init(options);
     var boardIsValid = board.validateBoard(gameBoard);
 
+    if(!boardIsValid) {
+      console.warn("Board is invalid :(");
+      return false;
+    }
+
     var append = function(element, value) {
       var newElement = element.cloneNode(true);
       boardArea.appendChild(newElement);
       return newElement;
     };
 
-    var appendCard = function(value) {
-      var element = append(template);
-      element.classList.add("card-" + board.random(1, 6));
-      element.addEventListener("click", function() {
-        this.classList.toggle("clicked");
-      });
-      element.getElementsByClassName("back")[0].innerHTML = value;
+    var lastCard = false;
+    var cardClicked = function() {
+      this.classList.toggle("clicked");
+
+      var isClicked = this.classList.contains("clicked");
+      var selectedCard = [this.dataset.row, this.dataset.card];
+
+      if(lastCard && isClicked) {
+        if(board.checkMatches([selectedCard, lastCard])) {
+          console.log("match!");
+        }
+      }
+
+      if(isClicked) {
+        lastCard = selectedCard;
+      } else {
+        lastCard = false;
+      }
+
+      console.log("text", board.checkCard(selectedCard[0], selectedCard[1]));
     };
 
-    for (var i = board.options.boardSize[0] - 1; i >= 0; i--) {
-      for (var u = board.options.boardSize[1] - 1; u >= 0; u--) {
-        appendCard(board.board[i][u]);
+    var appendCard = function(row, card) {
+      var element = append(template);
+      element.dataset.row = row;
+      element.dataset.card = card;
+
+      element.classList.add("card-" + board.random(1, 6));
+      element.addEventListener("click", cardClicked);
+      element.getElementsByClassName("back")[0].innerText = board.checkCard(row, card);
+    };
+
+    for (var row = board.options.boardSize[0] - 1; row >= 0; row--) {
+      for (var card = board.options.boardSize[1] - 1; card >= 0; card--) {
+        appendCard(row, card);
       }
       append(divider);
     }
